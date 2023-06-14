7e55f20ca0aad9f40c3d97b57706a285db4508f4
Levi Gruspe <mail.levig@gmail.com>
2023-06-14 18:56:26 +0800

    Simplify API to IPA clusterer

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,50 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2023 Levi Gruspe
 
-export {
-  Querier,
-  QueryOptions,
-  toQuerier,
-} from "./sound-change-parser/evaluate";
-export { parse } from "./sound-change-parser/parser";
-export {
-  Sound,
-  Environment,
-  Rule,
-  Context,
-  Ruleset,
-} from "./sound-change-parser/tree";
+import { cluster, Data } from "./ipa-cluster/cluster";
+import { CostFunction, levenshtein, Sequence } from "./ipa-cluster/metrics";
+import { parse, toQuerier } from "./sound-change-parser/index";
+
+// Create cost function for IPA clustering.
+// The sound changes in `code` are exempted from penalties when computing edit
+// distances.
+// May raise `ParseError`.
+function createCostFunction(code: string): CostFunction<string> {
+  const tree = parse(code);
+  const hasRule = toQuerier(tree);
+
+  return (s: Sequence<string>, t: Sequence<string>, i: number, j: number) => {
+    const a = s[i] || "";
+    const b = t[j] || "";
+
+    if (a === b) {
+      return 0;
+    }
+
+    // TODO set languages
+    const context = { left: "*", right: "*" };
+
+    const environmentS = {
+      before: s[i - 1] || "#",
+      after: s[i + 1] || "#",
+    };
+    const environmentT = {
+      before: t[j - 1] || "#",
+      after: t[j + 1] || "#",
+    };
+
+    const optionsS = { context, environment: environmentS };
+    const optionsT = { context, environment: environmentT };
+    return hasRule(a, b, optionsS) ? 0 : hasRule(a, b, optionsT) ? 0 : 1;
+  };
+}
+
+// Cluster words with similar IPA transcriptions together.
+// Uses the OPTICS clustering algorithm and the Levenshtein distance function.
+// `ignores` specifies edits/sound changes to not penalize.
+// May raise `ParseError`.
+export function clusterByIPA(dataset: Data[], ignores: string): Data[][] {
+  const cost = createCostFunction(ignores);
+  const metric = (a: string[], b: string[]) => levenshtein(a, b, cost);
+  return cluster(dataset, metric);
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,37 +1,31 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2023 Levi Gruspe
 
-import { createCostFunction } from "../src/cost";
-import { cluster, Data } from "../src/ipa-cluster/cluster";
-import { levenshtein } from "../src/ipa-cluster/metrics";
+import { Data } from "../src/ipa-cluster/cluster";
+
+import { clusterByIPA } from "../src/index";
 
 import { strict as assert } from "assert";
 
-describe("createCostFunction", () => {
+describe("clusterByIPA", () => {
   const dataset = [{ ipa: "q w e" }, { ipa: "a s d" }, { ipa: "z x c" }];
 
   describe("without ignores", () => {
-    it("smoke test", () => {
-      const cost = createCostFunction("");
-      const metric = (a: string[], b: string[]) => levenshtein(a, b, cost);
-      const clusters = cluster(dataset, metric);
-
+    it("should penalize unequal IPA segments", () => {
+      const clusters = clusterByIPA(dataset, "");
       assert.equal(clusters.length, 3);
     });
   });
 
   describe("with ignores", () => {
-    it("smoke test", () => {
+    it("should not penalize edits specified in the ruleset", () => {
       const code = `
         -- Sound changes work like types of edits to ignore.
         q -> a
         w -> s
         e -> d
       `;
-      const cost = createCostFunction(code);
-      const metric = (a: string[], b: string[]) => levenshtein(a, b, cost);
-      const clusters = cluster(dataset, metric);
-
+      const clusters = clusterByIPA(dataset, code);
       clusters.sort((a: Data[], b: Data[]) => a.length - b.length);
 
       // The code is written to cause "qwe" and "asd" to be clustered together.
