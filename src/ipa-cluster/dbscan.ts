050eab7b88fbace2d7a00b829e604c72f34fbd3c
Levi Gruspe <mail.levig@gmail.com>
2023-06-28 22:12:21 +0800

    Add function for suggesting dbscan epsilon

--- src/ipa-cluster/dbscan.ts
+++ src/ipa-cluster/dbscan.ts
@@ -3,7 +3,7 @@
 
 // Implements a simplified DBSCAN (clustering algorithm).
 
-import { StaticDisjointSet } from "mnemonist";
+import { StaticDisjointSet, VPTree } from "mnemonist";
 
 // Simplified DBSCAN implementation with minPoints = 2.
 // Returns array of arrays of clusters (as indices).
@@ -23,7 +23,38 @@ export function dbscan<T>(
       }
     }
   }
-
-  // TODO compute recommended epsilon using mnemonist/vp-tree
   return sets.compile();
+}
+
+// Suggests an epsilon value to use with dbscan according to the "elbow"
+// method.
+export function suggestEpsilon<T>(
+  dataset: T[],
+  distance: (a: T, b: T) => number
+): number {
+  // A `VPTree` can be constructed in O(nlogn).
+  const tree = VPTree.from(dataset, distance);
+
+  // Compute distances of 2nd nearest neighbors.
+  // This takes around O(nlogn) (O(logn) for each data point).
+  const distances = [];
+  for (const data of dataset) {
+    const neighbors = tree.nearestNeighbors(2, data);
+    distances.push(neighbors[neighbors.length - 1].distance);
+  }
+  distances.sort((a, b) => a - b);
+
+  // Find the elbow/point with sharpest slope.
+  let maxSlope = 0;
+  let index = 0;
+  for (let i = 1; i < distances.length; i++) {
+    const slope = distances[i] - distances[i - 1];
+    if (slope > maxSlope) {
+      maxSlope = slope;
+      index = i;
+    }
+  }
+
+  // Suggest the elbow.
+  return distances[index] || 0;
 }
