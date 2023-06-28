9a9cb0a935383083ea5d6d423eff3f308538b237
Levi Gruspe <mail.levig@gmail.com>
2023-06-28 11:38:50 +0800

    Test if every data point is clustered

--- test/ipa-cluster/cluster.test.ts
+++ test/ipa-cluster/cluster.test.ts
@@ -8,6 +8,8 @@ import { levenshtein } from "../../src/ipa-cluster/metrics";
 
 import { strict as assert } from "assert";
 
+import fc from "fast-check";
+
 describe("cluster", () => {
   it("smoke test", () => {
     const dataset = [{ ipa: "f o o" }, { ipa: "b a r" }, { ipa: "b a z" }];
@@ -18,5 +20,29 @@ describe("cluster", () => {
     // The clusters should be foo and bar-baz.
     assert.equal(clusters.length, 2);
     assert.deepEqual(clusters[0], [{ ipa: "f o o" }]);
+  });
+
+  it("should assign every word to a cluster", () => {
+    const metric = (p: Point, q: Point) => levenshtein(p.ipa, q.ipa);
+    const options = { maxLength: 10 };
+
+    fc.assert(
+      fc.property(
+        fc.array(fc.array(fc.unicode(), options), options),
+        (ipas: string[][]) => {
+          const dataset = ipas.map((tokens) => {
+            return { ipa: tokens.join(" ") };
+          });
+          const clusters = cluster(dataset, metric);
+
+          // Make sure all data points are assigned to a cluster.
+          let total = 0;
+          for (const cluster of clusters) {
+            total += cluster.length;
+          }
+          assert.equal(total, ipas.length);
+        }
+      )
+    );
   });
 });
