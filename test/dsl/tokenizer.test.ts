8a181b8f1f73d57f38b1315737f12668ede56549
Levi Gruspe <mail.levig@gmail.com>
2023-07-17 20:39:42 +0800

    Test word breakpoints

--- test/dsl/tokenizer.test.ts
+++ test/dsl/tokenizer.test.ts
@@ -46,4 +46,78 @@ describe("tokenize", () => {
     assert.strictEqual(tokenize("# foo")[0].tag, Tag.Terminal);
     assert.strictEqual(tokenize("#foo")[0].tag, Tag.Terminal);
   });
+
+  describe("words", () => {
+    it("should break words at reserved symbols", () => {
+      const examples: Array<[string, string[]]> = [
+        ["!foo!bar!baz!", ["!", "foo", "!", "bar", "!", "baz", "!"]],
+        ["$foo$bar$baz$", ["$", "foo", "$", "bar", "$", "baz", "$"]],
+        ["%foo%bar%baz%", ["%", "foo", "%", "bar", "%", "baz", "%"]],
+        ["&foo&bar&baz&", ["&", "foo", "&", "bar", "&", "baz", "&"]],
+        ["'foo'bar'baz'", ["'", "foo", "'", "bar", "'", "baz", "'"]],
+        ["(foo(bar(baz(", ["(", "foo", "(", "bar", "(", "baz", "("]],
+        [")foo)bar)baz)", [")", "foo", ")", "bar", ")", "baz", ")"]],
+        ["*foo*bar*baz*", ["*", "foo", "*", "bar", "*", "baz", "*"]],
+        ["+foo+bar+baz+", ["+", "foo", "+", "bar", "+", "baz", "+"]],
+        [",foo,bar,baz,", [",", "foo", ",", "bar", ",", "baz", ","]],
+        ["-foo-bar-baz-", ["-", "foo", "-", "bar", "-", "baz", "-"]],
+        [":foo:bar:baz:", [":", "foo", ":", "bar", ":", "baz", ":"]],
+        [";foo;bar;baz;", [";", "foo", ";", "bar", ";", "baz", ";"]],
+        ["<foo<bar<baz<", ["<", "foo", "<", "bar", "<", "baz", "<"]],
+        [">foo>bar>baz>", [">", "foo", ">", "bar", ">", "baz", ">"]],
+        ["?foo?bar?baz?", ["?", "foo", "?", "bar", "?", "baz", "?"]],
+        ["@foo@bar@baz@", ["@", "foo", "@", "bar", "@", "baz", "@"]],
+        ["[foo[bar[baz[", ["[", "foo", "[", "bar", "[", "baz", "["]],
+        ["\\foo\\bar\\baz\\", ["\\", "foo", "\\", "bar", "\\", "baz", "\\"]],
+        ["]foo]bar]baz]", ["]", "foo", "]", "bar", "]", "baz", "]"]],
+        ["^foo^bar^baz^", ["^", "foo", "^", "bar", "^", "baz", "^"]],
+        ["`foo`bar`baz`", ["`", "foo", "`", "bar", "`", "baz", "`"]],
+        ['"foo"bar"baz"', ['"', "foo", '"', "bar", '"', "baz", '"']],
+      ];
+
+      for (const [example, expected] of examples) {
+        assert.deepEqual(literals(tokenize(example)), expected);
+      }
+    });
+
+    it("should break words at separators", () => {
+      const examples: Array<[string, string[]]> = [
+        [".foo.bar.baz.", [".", "foo", ".", "bar", ".", "baz", "."]],
+        ["/foo/bar/baz/", ["/", "foo", "/", "bar", "/", "baz", "/"]],
+        ["=foo=bar=baz=", ["=", "foo", "=", "bar", "=", "baz", "="]],
+        ["\nfoo\nbar\nbaz\n", ["\n", "foo", "\n", "bar", "\n", "baz", "\n"]],
+        ["{foo{bar{baz{", ["{", "foo", "{", "bar", "{", "baz", "{"]],
+        ["|foo|bar|baz|", ["|", "foo", "|", "bar", "|", "baz", "|"]],
+        ["}foo}bar}baz}", ["}", "foo", "}", "bar", "}", "baz", "}"]],
+        ["~foo~bar~baz~", ["~", "foo", "~", "bar", "~", "baz", "~"]],
+      ];
+      for (const [example, expected] of examples) {
+        assert.deepEqual(literals(tokenize(example)), expected);
+      }
+    });
+
+    it("should break words at '#'", () => {
+      assert.deepEqual(literals(tokenize("#foo#bar#baz#")), [
+        "#",
+        "foo",
+        "#",
+        "bar",
+        "#",
+        "baz",
+        "#",
+      ]);
+    });
+
+    it("should break words at '_'", () => {
+      assert.deepEqual(literals(tokenize("_foo_bar_baz_")), [
+        "_",
+        "foo",
+        "_",
+        "bar",
+        "_",
+        "baz",
+        "_",
+      ]);
+    });
+  });
 });
