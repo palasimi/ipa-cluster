5382c0f1d0a8b059b3f24a04629689bd404bc7ad
Levi Gruspe <mail.levig@gmail.com>
2023-07-19 16:24:02 +0800

    Optimize alignment when padding sequence of segments

--- src/dsl/align.ts
+++ src/dsl/align.ts
@@ -8,6 +8,41 @@ import { SequenceSound } from "./operators";
 type Alignment = [SequenceSound, SequenceSound];
 
 /**
+ * Find the number of times that the shorter string should be "shifted" to the
+ * right to maximize the number of alignments (matching symbols) with the
+ * longer string.
+ */
+function findOptimalShift(left: SequenceSound, right: SequenceSound): number {
+  const short = left.length < right.length ? left : right;
+  const long = left.length < right.length ? right : left;
+
+  const m = long.length;
+  const n = short.length;
+
+  if (m === n) {
+    return 0;
+  }
+
+  // Find alignment with best score.
+  let maxScore = 0;
+  let bestStart = 0;
+  for (let start = 0; start < m - n; start++) {
+    let score = 0;
+    for (let i = 0; i < n; i++) {
+      if (short[i] === long[start + i]) {
+        score++;
+      }
+    }
+
+    if (score > maxScore) {
+      maxScore = score;
+      bestStart = start;
+    }
+  }
+  return bestStart;
+}
+
+/**
  * Pads left and right sequences with "_" or "#" so that the sequences are
  * aligned.
  */
@@ -19,11 +54,14 @@ function pad(left: SequenceSound, right: SequenceSound): Alignment {
   const long = left.length < right.length ? rightCopy : leftCopy;
 
   // Add padding to shorter sequence.
-  // TODO consider adding padding to the left to maximize number of aligned
-  // segments between the two sequences.
-  const padString = short[short.length - 1] === "#" ? "#" : "_";
+  const shift = findOptimalShift(short, long);
+  const leftPadString = short[0] === "#" ? "#" : "_";
+  for (let i = 0; i < shift; i++) {
+    short.unshift(leftPadString);
+  }
+  const rightPadString = short[short.length - 1] === "#" ? "#" : "_";
   while (short.length < long.length) {
-    short.push(padString);
+    short.push(rightPadString);
   }
   return [leftCopy, rightCopy];
 }
---
+++ src/dsl/index.ts
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Levi Gruspe
+
+export { compile, Querier } from "./compiler";
