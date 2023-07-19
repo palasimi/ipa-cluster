5c890c4e9a8c624c72041af68fe5c07fc7f5985f
Levi Gruspe <mail.levig@gmail.com>
2023-07-19 23:05:47 +0800

    Add cost function for new syntax

---
+++ src/newCost.ts
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023 Levi Gruspe
+
+import { CostFunction, CostFunctionOptions } from "./ipa-cluster/metrics";
+
+import { compile } from "./dsl/index";
+
+/**
+ * Creates cost function for IPA clustering.
+ */
+export function createCostFunction(code: string): CostFunction<string[]> {
+  const querier = compile(code);
+  return function customCost(
+    s: string[],
+    t: string[],
+    i: number,
+    j: number,
+    options: CostFunctionOptions = {}
+  ) {
+    const l1 = (options.left as string) || "_";
+    const l2 = (options.right as string) || "_";
+    return Number(querier.query(s, t, i, j, l1, l2));
+  };
+}
